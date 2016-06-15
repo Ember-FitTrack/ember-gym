5b41587b0999b5a96d7b1ea5f5ba63ce094f3b5a
Ethan <etp12@pitt.edu>
2016-06-14 20:12:03 -0400

    removed tutorial files

--- app/routes/programmers.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  model() {
-    return['Ethan P', 'Donald K', 'Bill G'];
-  }
-});
--- app/routes/scientists.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  model() {
-    return ['Ethan Pavolik', 'Ava Ball', 'Huh Huh'];
-  }
-});
