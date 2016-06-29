6875cac44689d6af205d2bd4d7a3ad9c2577855e
Ethan <etp12@pitt.edu>
2016-06-28 22:16:42 -0400

    /gym api working

--- app/controllers/gyms.js
+++ app/controllers/gyms.js
@@ -6,6 +6,7 @@ export default Ember.Controller.extend({
     findGym() {
       let self = this;
       let addr = this.get('gymAddress');
+      console.log('address: ' + addr);
       return this.get('ajax').request('/google-gym', {
         method: 'GET',
         data: {
@@ -15,6 +16,20 @@ export default Ember.Controller.extend({
       .then(function(res) {
         self.set('latitude', res.latitude);
         self.set('longitude', res.longitude);
+        return self.get('ajax').request('/gym', {
+          method: 'GET',
+          data: {
+            latitude: res.latitude,
+            longitude: res.longitude
+          }
+        })
+        .then(function(res) {
+          console.log('exists?');
+          console.log(res);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
       })
       .catch(function(err) {
         console.log(err);
--- app/models/gyms.js
+++ app/models/gyms.js
@@ -1,13 +1,10 @@
 import Model from 'ember-data/model';
-// import attr from 'ember-data/attr';
+import attr from 'ember-data/attr';
 // import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
   name: attr('string'),
   address: attr('string'),
-  geography: {
-    latitude: attr('number'),
-    longitude: attr('number')
-  }
+  latitude: attr('number'),
+  longitude: attr('number')
 });
- 
